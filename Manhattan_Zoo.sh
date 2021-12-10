6f1b92132e4668a61baaa328e7bca034d6060c13
Ascetic313 <iddrisualmuqadas74@gmail.com>
2021-12-10 10:03:39 +0000

    This is a project on Manhattan_Zoo

---
+++ Manhattan_Zoo.sh
@@ -0,0 +1,25 @@
+$ # Initializing a Git Repository:
+$ git init
+Reinitialized existing Git repository in /home/ccuser/workspace/manhattan-zoo-1/.git/
+$ 
+$ # Checking for Git Status:
+$ git status
+On branch master
+nothing to commit, working directory clean
+$ 
+$ # Adding meal-regiments.txt to staging area:
+$ git add meal-regimens.txt
+$ git commit -m " This is about meals regimens"
+On branch master
+nothing to commit, working directory clean
+$ git add meal-regimens.txt
+$ git status
+On branch master
+nothing to commit, working directory clean
+$ git log
+commit 0cc4d01ad28c35c6cbc89ddb10018d56130375d4
+Author: codecademy <ccuser@codecademy.com>
+Date:   Mon Oct 25 22:05:53 2021 +0000
+
+    This a great meal
+$ 
